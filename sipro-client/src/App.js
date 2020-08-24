d5cfc6af920152f6c6e7dd588b5c4bbbb7a0575b
JORGE.BARRIENTOSCERDAS <jorge.barrientoscerdas@ucr.ac.cr>
2020-08-24 16:19:26 -0600

    agregar buscador

---
+++ sipro-client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Buscador from './components/Buscador';
+
+function App() {
+  return (
+    <div className="App">
+      <Buscador />
+    </div>
+  );
+}
+
+export default App;
